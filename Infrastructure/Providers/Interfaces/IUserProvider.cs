2eaaaf4b296063a49b3ecc46fcb7fc4a79cbb002
Dalibor Homola <Dalibor_Homola@wistron.com>
2021-02-10 13:16:41 +0100

    add: userProvider

---
+++ Infrastructure/Providers/Interfaces/IUserProvider.cs
@@ -0,0 +1,12 @@
+﻿namespace PROJECT.Infrastructure.Providers.Interfaces
+{
+    public interface IUserProvider
+    {
+        /// <summary>
+        /// Get username with employeeId in format: "username (userId)"
+        /// </summary>
+        string Get();
+        string GetName();
+        string GetId();
+    }
+}
---
+++ Infrastructure/Providers/UserProvider.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using PROJECT.Infrastructure.Providers.Interfaces;
+
+namespace PROJECT.Infrastructure.Providers
+{
+    public class UserProvider : IUserProvider
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public UserProvider(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public string Get()
+        {
+            string username = _httpContextAccessor.HttpContext.User.FindFirst("name")?.Value;
+            string employeeId = _httpContextAccessor.HttpContext.User.FindFirst("employeeId")?.Value;
+
+            if (!string.IsNullOrEmpty(username) && !string.IsNullOrEmpty(employeeId))
+            {
+                return username + " (" + employeeId + ")";
+            }
+
+            return null;
+        }
+
+        public string GetName()
+        {
+            return _httpContextAccessor.HttpContext.User.FindFirst("name")?.Value;
+        }
+
+        public string GetId()
+        {
+            return _httpContextAccessor.HttpContext.User.FindFirst("employeeId")?.Value;
+        }
+    }
+}
