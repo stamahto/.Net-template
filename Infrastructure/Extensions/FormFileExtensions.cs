a376a1df9f196bc5170a7d72a0595134f2516b32
Dalibor Homola <57343418+stamahto@users.noreply.github.com>
2021-03-04 11:48:05 +0100

    Update FormFileExtensions.cs

--- Infrastructure/Extensions/FormFileExtensions.cs
+++ Infrastructure/Extensions/FormFileExtensions.cs
@@ -8,7 +8,7 @@ namespace PROJECT.Infrastructure.Extensions
     {
         public static async Task<byte[]> GetBytes(this IFormFile formFile)
         {
-            using MemoryStream memoryStream = new MemoryStream();
+            using MemoryStream memoryStream = new();
             await formFile.CopyToAsync(memoryStream);
             return memoryStream.ToArray();
         }
